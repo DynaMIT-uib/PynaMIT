993db0e205509bc0468953174476838cee4b5ddc
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-04-10 12:15:10 +0200

    Ensure lon0 is a scalar

--- src/pynamit/pynamit.py
+++ src/pynamit/pynamit.py
@@ -362,7 +362,7 @@ if __name__ == '__main__':
     d = dipole.Dipole(date.year)
 
     # noon longitude
-    lon0 = d.mlt2mlon(12, date)
+    lon0 = d.mlt2mlon(12, date).item()
 
     hall, pedersen = conductance.hardy_EUV(np.asnumpy(i2d.phi), np.asnumpy(90 - i2d.theta), Kp, date, starlight = 1, dipole = True)
     hall, pedersen = np.array(hall), np.array(pedersen)
