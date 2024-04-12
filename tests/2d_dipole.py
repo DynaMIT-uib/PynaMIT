d3fd73989735860125bd860643f0e8635b4376cf
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-04-12 15:12:51 +0200

    Move

--- tests/2d_dipole.py
+++ tests/2d_dipole.py
@@ -11,19 +11,19 @@ def test_run_pynamit():
     expected_coeff_min = -5.040932467648457e-18
     expected_n_coeffs = 201
 
-    # Act
     temp_dir = os.path.join(tempfile.gettempdir(), "test_run_pynamit")
     if not os.path.exists(temp_dir):
         os.mkdir(temp_dir)
 
+    # Act
     coeffs = run_pynamit(totalsteps=200, dt=5e-4, Nmax=45, Mmax=3, Ncs=60, fig_directory=temp_dir)
 
+    # Assert
     actual_coeff_norm = np.linalg.norm(coeffs)
     actual_coeff_max = np.max(coeffs)
     actual_coeff_min = np.min(coeffs)
     actuall_n_coeffs = len(coeffs)
- 
-    # Assert
+
     assert actual_coeff_norm == expected_coeff_norm
     assert actual_coeff_max == expected_coeff_max
     assert actual_coeff_min == expected_coeff_min
