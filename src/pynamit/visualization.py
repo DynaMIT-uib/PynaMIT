cd1c573361da947b7dd5ddf27ec36a01171ea830
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-04-19 11:15:15 +0200

    Reverted to some of the old structure while simplifying

--- src/pynamit/visualization.py
+++ src/pynamit/visualization.py
@@ -125,7 +125,6 @@ if __name__ == "__main__":
     lat, lon = np.meshgrid(lat, lon)
 
     import pyamps
-    from pynamit.visualization.visualization import globalplot
     import matplotlib.pyplot as plt
     from lompe import conductance
     import dipole
