15a60e030c9e11b0b1f82bd43b55c84d6be18f40
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-04-24 09:39:41 +0200

    Try to fix last (?) indentation issue

--- src/sh_utils/sh_utils.py
+++ src/sh_utils/sh_utils.py
@@ -1,6 +1,5 @@
 """
-Tools that are useful for spherical harmonic analysis
-
+Tools that are useful for spherical harmonic analysis:
     SHkeys       -- class to contain n and m - the indices of the spherical harmonic terms
     nterms       -- function which calculates the number of terms in a 
                     real expansion of a poloidal (internal + external) and toroidal expansion 
