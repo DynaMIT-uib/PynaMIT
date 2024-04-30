820160148b24080275c31b884fb69b6b6d36c3bc
Karl Laundal <karl.laundal@ift.uib.no>
2024-04-30 20:55:45 +0200

    fixed PFAC script so that it works with new structure

--- scripts/test_PFAC.py
+++ scripts/test_PFAC.py
@@ -12,7 +12,7 @@ from lompe import conductance
 
 reload(pynamit)
 RE = 6371.2e3
-
+RI = RE + 110e3
 
 # MODEL PARAMETERS
 Nmax, Mmax, Ncs = 45, 20, 40
@@ -26,43 +26,50 @@ Wlevels = np.r_[-512.5:512.5:5]
 Philevels = np.r_[-212.5:212.5:5]
 
 ## SET UP SIMULATION OBJECT
-i2d = pynamit.I2D(Nmax, Mmax, Ncs, B0 = 'dipole', FAC_integration_parameters = {'steps':np.logspace(np.log10(RE + 110.e3), np.log10(7 * RE), 11)})
+i2d_sha = pynamit.sha(Nmax, Mmax)
+i2d_csp = pynamit.CSprojection(Ncs)
+i2d = pynamit.I2D(i2d_sha, i2d_csp, RI, mainfield_kind = 'dipole', FAC_integration_parameters = {'steps':np.logspace(np.log10(RI), np.log10(7 * RE), 11)}, ignore_PNAF = False)
 
+## SET UP PLOTTING GRID
+lat, lon = np.linspace(-89.9, 89.9, Ncs * 2), np.linspace(-180, 180, Ncs * 4)
+lat, lon = np.meshgrid(lat, lon)
+plt_grid = pynamit.grid.grid(RI, lat, lon)
+plt_grid.construct_G(i2d_sha)
 
 ## CONDUCTANCE AND FAC INPUT:
 date = datetime.datetime(2001, 5, 12, 21, 45)
 Kp   = 5
 d = dipole.Dipole(date.year)
 lon0 = d.mlt2mlon(12, date) # noon longitude
-hall, pedersen = conductance.hardy_EUV(i2d.phi, 90 - i2d.theta, Kp, date, starlight = 1, dipole = True)
-i2d.set_conductance(hall, pedersen)
+hall, pedersen = conductance.hardy_EUV(i2d.num_grid.lon, i2d.num_grid.lat, Kp, date, starlight = 1, dipole = True)
+i2d.state.set_conductance(hall, pedersen)
 
 a = pyamps.AMPS(300, 0, -4, 20, 100, minlat = 50)
-jparallel = -a.get_upward_current(mlat = 90 - i2d.theta, mlt = d.mlon2mlt(i2d.phi, date)) / i2d.sinI * 1e-6
-jparallel[np.abs(90 - i2d.theta) < 50] = 0 # filter low latitude FACs
+jparallel = -a.get_upward_current(mlat = i2d.num_grid.lat, mlt = d.mlon2mlt(i2d.num_grid.lon, date)) / i2d.state.sinI * 1e-6
+jparallel[np.abs(i2d.num_grid.lat) < 50] = 0 # filter low latitude FACs
 
 
-i2d.set_FAC(jparallel)
+i2d.state.set_FAC(jparallel)
 
 print('Building SECS matrices. This takes some time (and memory) because of global grids...')
-secsI = jparallel * i2d.sinI * i2d.csp.unit_area * i2d.RI**2 # SECS amplitudes are downward current density times area
-lat, lon = i2d.lat.flatten(), i2d.lon.flatten()
-r = np.full(lat.size, i2d.RI - 1)
-lat_secs, lon_secs = 90 - i2d.theta, i2d.phi
-Be, Bn, Br = i2d.bphi, - i2d.btheta, i2d.br
-Ge, Gn, Gu = secsy.get_CF_SECS_B_G_matrices_for_inclined_field(lat, lon, r, lat_secs, lon_secs, Be, Bn, Br, RI = i2d.RI)
+secsI = jparallel * i2d.state.sinI * i2d_csp.unit_area * i2d.num_grid.RI**2 # SECS amplitudes are downward current density times area
+lat, lon = plt_grid.lat.flatten(), plt_grid.lon.flatten()
+r = np.full(lat.size, i2d.num_grid.RI - 1)
+lat_secs, lon_secs = i2d.num_grid.lat, i2d.num_grid.lon
+Be, Bn, Br = i2d.state.bphi, - i2d.state.btheta, i2d.state.br
+Ge, Gn, Gu = secsy.get_CF_SECS_B_G_matrices_for_inclined_field(lat, lon, r, lat_secs, lon_secs, Be, Bn, Br, RI = i2d.num_grid.RI)
 
 
 Br_SECS = Gu.dot(secsI)
 
-GBr = i2d.Gplt * i2d.n / i2d.RI
-Br_I2D = GBr.dot(i2d.shc_PFAC)
+GBr = plt_grid.G * i2d_sha.n / i2d.num_grid.RI
+Br_I2D = GBr.dot(i2d.state.shc_PFAC)
 
 
-fig, paxn, paxs, axg =  pynamit.globalplot(i2d.lon, i2d.lat, Br_I2D.reshape(i2d.lat.shape), returnplot = True, 
+fig, paxn, paxs, axg =  pynamit.globalplot(plt_grid.lon, plt_grid.lat, Br_I2D.reshape(plt_grid.lat.shape), returnplot = True, 
                                            levels = Blevels, cmap = 'bwr', noon_longitude = lon0, extend = 'both')
 
-fig, paxn, paxs, axg =  pynamit.globalplot(i2d.lon, i2d.lat, Br_SECS.reshape(i2d.lat.shape), returnplot = True, 
+fig, paxn, paxs, axg =  pynamit.globalplot(plt_grid.lon, plt_grid.lat, Br_SECS.reshape(plt_grid.lat.shape), returnplot = True, 
                                            levels = Blevels, cmap = 'bwr', noon_longitude = lon0, extend = 'both')
 
 fig, ax = plt.subplots(figsize = (10, 10))
