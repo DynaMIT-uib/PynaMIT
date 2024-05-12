4545e2db56b26f6caa060add96cf93c3d80de217
Karl Laundal <karl.laundal@ift.uib.no>
2024-05-12 21:49:35 +0200

    added import of debugplot

--- src/pynamit/__init__.py
+++ src/pynamit/__init__.py
@@ -1,5 +1,5 @@
 from .pynamit import I2D
-from .visualization import globalplot
+from .visualization import globalplot, debugplot
 from .cubedsphere.cubedsphere import CSProjection
 from .sha.sh_basis import SHBasis
 from .mainfield import Mainfield
