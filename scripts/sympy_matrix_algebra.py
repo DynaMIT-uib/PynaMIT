8e1eb77992bce10f49b8445ad1baf2908b11bd00
Karl Laundal <karl.laundal@ift.uib.no>
2024-05-12 12:49:30 +0200

    cleaned up sympy script

--- scripts/sympy_matrix_algebra.py
+++ scripts/sympy_matrix_algebra.py
@@ -3,7 +3,7 @@
 import sympy as sp
 #sp.init_printing(use_unicode=True)
 
-PRINT_LATEX = True # set to True to print LaTeX, False to print something more code-friendly
+PRINT_LATEX = False # set to True to print LaTeX, False to print something more code-friendly
 
 if PRINT_LATEX:
     br, bt, bp, ut, up, eP, eH, B0, Br = sp.symbols('b_r, b_theta, b_phi, u_theta, u_phi, eta_P, eta_H, B_0, B_r')
@@ -56,79 +56,29 @@ if PRINT_LATEX:
     print('=&                 \\eta_P\\alpha_{22}^{\\eta_P}                    && + \\eta_H\\alpha_{22}^{\\eta_H}                         \\\\')
     print('\\alpha_{13}=&' + 'u_\\theta('+ sp.latex(alpha23.coeff(ut)) + ')    && + u_\\phi('+ sp.latex(alpha23.coeff(up)) + ')\\nonumber \\\\')
     print('=&                 u_\\theta\\alpha_{13}^{u_\\theta}                && + u_\\phi\\alpha_{13}^{u_\\phi}                         \\\\')
-    print('\\alpha_{23}=&' + 'u_\\theta('+ sp.latex(alpha23.coeff(ut)) + ')   && + u_\\phi('+ sp.latex(alpha23.coeff(up)) + ')\\nonumber \\\\')
+    print('\\alpha_{23}=&' + 'u_\\theta('+ sp.latex(alpha23.coeff(ut)) + ')    && + u_\\phi('+ sp.latex(alpha23.coeff(up)) + ')\\nonumber \\\\')
     print('=&                 u_\\theta\\alpha_{23}^{u_\\theta}                && + u_\\phi\\alpha_{23}^{u_\\phi}                             ')
 
 
-print(3/0)
-
-
-
-
-
-
-#print('A4:')
-#print(sp.latex(A4))
-
-A400 = sp.collect(sp.factor(A4[0, 0]), [eP, eH, ut, up]).expand()
-A401 = sp.collect(sp.factor(A4[0, 1]), [eP, eH, ut, up]).expand()
-A402 = sp.collect(sp.factor(A4[0, 2]), [eP, eH, ut, up]).expand()
-A410 = sp.collect(sp.factor(A4[1, 0]), [eP, eH, ut, up]).expand()
-A411 = sp.collect(sp.factor(A4[1, 1]), [eP, eH, ut, up]).expand()
-A412 = sp.collect(sp.factor(A4[1, 2]), [eP, eH, ut, up]).expand()
-
-A4_pars = []
-for p in [eP, eH, ut, up]:
-    A4_pars.append(sp.Matrix([[(A400.coeff(p)).simplify(), (A401.coeff(p)).simplify(), (A402.coeff(p)).simplify()],
-                              [(A410.coeff(p)).simplify(), (A411.coeff(p)).simplify(), (A412.coeff(p)).simplify()]]))
-
-if PRINT_LATEX:
-    labels = ['{\\eta_P}', '{\\eta_H}', '{u_\\theta}', '{u_\\phi}']
-    for i in range(4):
-        print('\\mathbb A4_' + labels[i] + '=&')
-        print(sp.latex(sp.simplify(A4_pars[i])))
-        print('\\\\')
-    print('\n\n')
-
-    for i in range(2):
-        print('\\mathbb A5_' + labels[i] + '=&')
-        print(sp.latex(sp.simplify(A4_pars[i][:, :-1])))
-        print('\\\\')
-    print('\n\n')
-
-    print('\\mathbf{c} = u_\\theta' + sp.latex(sp.simplify(A4_pars[2][:, -1])) + '+ u_\\phi' + sp.latex(sp.simplify(A4_pars[3][:, -1])))
-
-
-
-# Write A5 and c, which are just A4 split up:
-A5_eP, A5_eH = [A4_pars[0][:,: -1], A4_pars[1][:,: -1]]
-c_ut, c_up   = [A4_pars[2][:,  -1], A4_pars[3][:,  -1]]
-
 if not PRINT_LATEX: # print code:
     print('CODE:\n\n')
-    print('# constant that is to be multiplied by u in theta direction:')
-    print('c_ut_theta = ' + str(sp.simplify(c_ut[0])))
-    print('c_ut_phi   = ' + str(sp.simplify(c_ut[1])))
-    print('c_ut = np.hstack((c_ut_theta, c_ut_phi)).reshape((-1, 1)) # stack and convert to column vector\n')
+    print('# resistance terms:')
+    print('alpha11_eP = ' + str(alpha11.coeff(eP)))
+    print('alpha12_eP = ' + str(alpha12.coeff(eP)))
+    print('alpha21_eP = ' + str(alpha21.coeff(eP)))
+    print('alpha22_eP = ' + str(alpha22.coeff(eP)))
+    print('alpha11_eH = ' + str(alpha11.coeff(eH)))
+    print('alpha12_eH = ' + str(alpha12.coeff(eH)))
+    print('alpha21_eH = ' + str(alpha21.coeff(eH)))
+    print('alpha22_eH = ' + str(alpha22.coeff(eH)))
 
-    print('# constant that is to be multiplied by u in phi direction:')
-    print('c_up_theta = ' + str(sp.simplify(c_up[0])))
-    print('c_up_phi   = ' + str(sp.simplify(c_up[1])))
-    print('c_up = np.hstack((c_up_theta, c_up_phi)).reshape((-1, 1)) # stack and convert to column vector\n\n')
+    print('\n# wind terms:')
+    print('alpha13_ut = ' + str(alpha13.coeff(ut)))
+    print('alpha23_ut = ' + str(alpha23.coeff(ut)))
+    print('alpha13_up = ' + str(alpha13.coeff(up)))
+    print('alpha23_up = ' + str(alpha23.coeff(up)))
 
-    print('# A5eP matrix')
-    print('a5eP00 = spr.diags(' + str(A5_eP[0, 0]) + ')') 
-    print('a5eP01 = spr.diags(' + str(A5_eP[0, 1]) + ')') 
-    print('a5eP10 = spr.diags(' + str(A5_eP[1, 0]) + ')') 
-    print('a5eP11 = spr.diags(' + str(A5_eP[1, 1]) + ')')
-    print('a5eP = spr.vstack((spr.hstack((a5eP00, a5eP01)), spr.hstack((a5eP10, a5eP11)))).tocsr()\n')
 
-    print('# A5eH matrix')
-    print('a5eH00 = spr.diags(' + str(A5_eH[0, 0]) + ')') 
-    print('a5eH01 = spr.diags(' + str(A5_eH[0, 1]) + ')') 
-    print('a5eH10 = spr.diags(' + str(A5_eH[1, 0]) + ')') 
-    print('a5eH11 = spr.diags(' + str(A5_eH[1, 1]) + ')')
-    print('a5eH = spr.vstack((spr.hstack((a5eH00, a5eH01)), spr.hstack((a5eH10, a5eH11)))).tocsr()')
 
 
 
