341f25e9b1d983a3b3176ad3d4a6b061f251dcc8
Karl Laundal <karl.laundal@ift.uib.no>
2024-05-13 13:03:16 +0200

    resolved conflict

--- scripts/figlayout.py
+++ scripts/figlayout.py
@@ -14,10 +14,13 @@ import pyamps
 
 RE = 6371.2e3
 RI = RE + 110e3
-latitude_boundary = 40
+latitude_boundary = 35
 
-Nmax, Mmax, Ncs = 25, 25, 50
-rk = RI / np.cos(np.deg2rad(np.r_[0: 60: int(180 / Nmax) - 1])) ** 2
+WIND_FACTOR = 1 # scale wind by this factor
+
+Nmax, Mmax, Ncs = 14, 14, 30
+rk = RI / np.cos(np.deg2rad(np.r_[0: 80: int(80 / Nmax)])) ** 2
+print(len(rk))
 rk = {'steps':rk}
 date = datetime.datetime(2001, 5, 12, 21, 45)
 Kp   = 5
@@ -54,114 +57,127 @@ a = pyamps.AMPS(300, 0, -4, 20, 100, minlat = 50)
 jparallel = -a.get_upward_current(mlat = csp_grid.lat, mlt = d.mlon2mlt(csp_grid.lon, date)) / i2d.state.sinI * 1e-6
 jparallel[np.abs(csp_grid.lat) < 50] = 0 # filter low latitude FACs
 
-i2d.state.set_u(-u_north_int, u_east_int)
+i2d.state.set_u(-u_north_int * WIND_FACTOR, u_east_int * WIND_FACTOR)
 i2d.state.set_FAC(jparallel, csp_sh_evaluator)
 
 
 #### MODEL OBJECT DONE
+def debugplot(i2d, title = None, filename = None, noon_longitude = 0):
+
+    ## SET UP PLOTTING GRID
+    lat, lon = np.linspace(-89.9, 89.9, Ncs * 2), np.linspace(-180, 180, Ncs * 4)
+    lat, lon = np.meshgrid(lat, lon)
+    plt_grid = pynamit.grid.Grid(RI, lat, lon)
+    plt_sh_evaluator = pynamit.basis_evaluator.BasisEvaluator(i2d_sh, plt_grid)
+
+
+    B_kwargs   = {'cmap':plt.cm.bwr, 'levels':np.linspace(-50, 50, 22) * 1e-9, 'extend':'both'}
+    eqJ_kwargs = {'colors':'black', 'levels':np.r_[-210:220:20] * 1e-3}
+    FAC_kwargs = {'cmap':plt.cm.bwr, 'levels':np.linspace(-.95, .95, 22) * 1e-6, 'extend':'both'}
+
+
+    ## SET UP PLOTTING GRID AND BASIS EVALUATOR
+    NLA, NLO = 50, 90
+    lat, lon = np.linspace(-89.9, 89.9, NLA), np.linspace(-180, 180, NLO)
+    lat, lon = map(np.ravel, np.meshgrid(lat, lon))
+    plt_grid = pynamit.grid.Grid(i2d.state.RI, lat, lon)
+    plt_sh_evaluator = pynamit.basis_evaluator.BasisEvaluator(i2d.state.sh, plt_grid)
+
+    ## MAP PROJECTION:
+    global_projection = ccrs.PlateCarree(central_longitude = noon_longitude)
+
+    fig = plt.figure(figsize = (15, 13))
+
+    paxn_B = Polarplot(plt.subplot2grid((4, 4), (0, 0)))
+    paxs_B = Polarplot(plt.subplot2grid((4, 4), (0, 1)))
+    paxn_j = Polarplot(plt.subplot2grid((4, 4), (0, 2)))
+    paxs_j = Polarplot(plt.subplot2grid((4, 4), (0, 3)))
+    gax_B = plt.subplot2grid((4, 2), (1, 0), projection = global_projection, rowspan = 2)
+    gax_j = plt.subplot2grid((4, 2), (1, 1), projection = global_projection, rowspan = 2)
+    ax_1 = plt.subplot2grid((4, 3), (3, 0))
+    ax_2 = plt.subplot2grid((4, 3), (3, 1))
+    ax_3 = plt.subplot2grid((4, 3), (3, 2))
+
+    for ax in [gax_B, gax_j]: 
+        ax.coastlines(zorder = 2, color = 'grey')
+
+    ## CALCULATE VALUES TO PLOT
+    Br  = i2d.state.get_Br(plt_sh_evaluator)
+
+    sinI   = i2d.state.mainfield.get_sinI(plt_grid.RI, plt_grid.theta, plt_grid.lon)
+    FAC    = -(plt_sh_evaluator.scaled_G(1 / sinI.reshape((-1, 1)))).dot(i2d.state.TJr.coeffs)
+    jr_mod =   plt_sh_evaluator.G.dot(i2d.state.TJr.coeffs)
+    eq_current_function = plt_sh_evaluator.G.dot(i2d.state.VJ.coeffs)
+
+    ## GLOBAL PLOTS
+    gax_B.contourf(lon.reshape((NLO, NLA)), lat.reshape((NLO, NLA)), Br.reshape((NLO, NLA)), transform = ccrs.PlateCarree(), **B_kwargs)
+    gax_j.contour( lon.reshape((NLO, NLA)), lat.reshape((NLO, NLA)), eq_current_function.reshape((NLO, NLA)), transform = ccrs.PlateCarree(), **eqJ_kwargs)
+    gax_j.contourf(lon.reshape((NLO, NLA)), lat.reshape((NLO, NLA)), FAC.reshape((NLO, NLA)), **FAC_kwargs)
+
+
+    ## POLAR PLOTS
+    mlt = (lon - noon_longitude + 180) / 15 # rotate so that noon is up
+
+    # north:
+    iii = lat >  50
+    paxn_B.contourf(lat[iii], mlt[iii], Br[iii], **B_kwargs)
+    paxn_j.contour( lat[iii], mlt[iii], eq_current_function[iii], **eqJ_kwargs)
+    paxn_j.contourf(lat[iii], mlt[iii], FAC[iii], **FAC_kwargs)
+
+    # south:
+    iii = lat < -50
+    paxs_B.contourf(lat[iii], mlt[iii], Br[iii], **B_kwargs)
+    paxs_j.contour( lat[iii], mlt[iii], eq_current_function[iii], **eqJ_kwargs)
+    paxs_j.contourf(lat[iii], mlt[iii], FAC[iii], **FAC_kwargs)
+
+
+    # scatter plot high latitude FACs
+    iii = np.abs(i2d.state.num_grid.lat) > i2d.state.latitude_boundary
+    jrmax = np.max(np.abs(i2d.state.jr))
+    ax_1.scatter(i2d.state.jr, jr_mod[iii])
+    ax_1.plot([-jrmax, jrmax], [-jrmax, jrmax], 'k-')
+    ax_1.set_xlabel('Input ')
+
+    # scatter plot FACs at conjugate points
+    j_par_ll = i2d.state.G_par_ll.dot(i2d.state.TB.coeffs)
+    j_par_cp = i2d.state.G_par_cp.dot(i2d.state.TB.coeffs)
+    j_par_max = np.max(np.abs(j_par_ll))
+    ax_2.scatter(j_par_ll, j_par_cp)
+    ax_2.plot([-j_par_max, j_par_max], [-j_par_max, j_par_max], 'k-')
+    ax_2.set_xlabel('$j_\parallel$ [A/m$^2$] at |latitude| $< {}^\circ$'.format(i2d.state.latitude_boundary))
+    ax_2.set_ylabel('$j_\parallel$ [A/m$^2$] at conjugate points')
+
+    # scatter plot of Ed1 and Ed2 vs corresponding values at conjugate points
+    cu_ll = i2d.state.u_phi_cp * i2d.state.aup_cp   + i2d.state.u_theta_cp * i2d.state.aut_cp
+    cu_cp = i2d.state.u_phi_ll * i2d.state.aup_ll   + i2d.state.u_theta_ll * i2d.state.aut_ll
+    AT_ll = i2d.state.etaP_ll  * i2d.state.aeP_T_ll + i2d.state.etaH_ll    * i2d.state.aeH_T_ll
+    AT_cp = i2d.state.etaP_cp  * i2d.state.aeP_T_cp + i2d.state.etaH_cp    * i2d.state.aeH_T_cp
+    AV_ll = i2d.state.etaP_ll  * i2d.state.aeP_V_ll + i2d.state.etaH_ll    * i2d.state.aeH_V_ll
+    AV_cp = i2d.state.etaP_cp  * i2d.state.aeP_V_cp + i2d.state.etaH_cp    * i2d.state.aeH_V_cp
+
+    c_ll = cu_ll + AV_ll.dot(i2d.state.VB.coeffs)
+    c_cp = cu_cp + AV_cp.dot(i2d.state.VB.coeffs)
+    Ed1_ll, Ed2_ll = np.split(c_ll + AT_ll.dot(i2d.state.TB.coeffs), 2)
+    Ed1_cp, Ed2_cp = np.split(c_cp + AT_cp.dot(i2d.state.TB.coeffs), 2)
+    ax_3.scatter(Ed1_ll, Ed1_cp, label = '$E_{d_1}$')
+    ax_3.scatter(Ed2_ll, Ed2_cp, label = '$E_{d_2}$')
+    ax_3.set_xlabel('$E_{d_i}$')
+    ax_3.set_ylabel('$E_{d_i}$ at conjugate points')
+    ax_3.legend(frameon = False)
+
+    if title is not None:
+        gax_j.set_title(title)
+
+    plt.subplots_adjust(top=0.89, bottom=0.095, left=0.025, right=0.95, hspace=0.0, wspace=0.185)
+    if filename is not None:
+        fig.savefig(filename)
+    else:
+        plt.show()
 
 
 
 
-B_kwargs   = {'cmap':plt.cm.bwr, 'levels':np.linspace(-50, 50, 22) * 1e-9, 'extend':'both'}
-eqJ_kwargs = {'colors':'black', 'levels':np.r_[-210:220:20] * 1e-3}
-FAC_kwargs = {'cmap':plt.cm.bwr, 'levels':np.linspace(-.95, .95, 22) * 1e-6, 'extend':'both'}
-
-
-## SET UP PLOTTING GRID AND BASIS EVALUATOR
-NLA, NLO = 50, 90
-lat, lon = np.linspace(-89.9, 89.9, NLA), np.linspace(-180, 180, NLO)
-lat, lon = map(np.ravel, np.meshgrid(lat, lon))
-plt_grid = pynamit.grid.Grid(i2d.state.RI, lat, lon)
-plt_sh_evaluator = pynamit.basis_evaluator.BasisEvaluator(i2d.state.sh, plt_grid)
-
-## MAP PROJECTION:
-global_projection = ccrs.PlateCarree(central_longitude = noon_longitude)
-
-fig = plt.figure(figsize = (15, 13))
-
-paxn_B = Polarplot(plt.subplot2grid((4, 4), (0, 0)))
-paxs_B = Polarplot(plt.subplot2grid((4, 4), (0, 1)))
-paxn_j = Polarplot(plt.subplot2grid((4, 4), (0, 2)))
-paxs_j = Polarplot(plt.subplot2grid((4, 4), (0, 3)))
-gax_B = plt.subplot2grid((4, 2), (1, 0), projection = global_projection, rowspan = 2)
-gax_j = plt.subplot2grid((4, 2), (1, 1), projection = global_projection, rowspan = 2)
-ax_1 = plt.subplot2grid((4, 3), (3, 0))
-ax_2 = plt.subplot2grid((4, 3), (3, 1))
-ax_3 = plt.subplot2grid((4, 3), (3, 2))
-
-for ax in [gax_B, gax_j]:
-    ax.coastlines(zorder = 2, color = 'grey')
-
-## CALCULATE VALUES TO PLOT
-Br  = plt_sh_evaluator.scaled_G(i2d.state.sh.n / i2d.state.num_grid.RI).dot(i2d.state.VB.coeffs)
-
-sinI   = i2d.state.mainfield.get_sinI(plt_grid.RI, plt_grid.theta, plt_grid.lon)
-FAC    = -(plt_sh_evaluator.scaled_G(1 / sinI.reshape((-1, 1)))).dot(i2d.state.TJr.coeffs)
-jr_mod =   csp_sh_evaluator.G.dot(i2d.state.TJr.coeffs)
-eq_current_function = plt_sh_evaluator.G.dot(i2d.state.VJ.coeffs)
-
-## GLOBAL PLOTS
-gax_B.contourf(lon.reshape((NLO, NLA)), lat.reshape((NLO, NLA)), Br.reshape((NLO, NLA)), transform = ccrs.PlateCarree(), **B_kwargs)
-gax_j.contour( lon.reshape((NLO, NLA)), lat.reshape((NLO, NLA)), eq_current_function.reshape((NLO, NLA)), transform = ccrs.PlateCarree(), **eqJ_kwargs)
-gax_j.contourf(lon.reshape((NLO, NLA)), lat.reshape((NLO, NLA)), FAC.reshape((NLO, NLA)), **FAC_kwargs)
-
-
-## POLAR PLOTS
-mlt = (lon - noon_longitude + 180) / 15 # rotate so that noon is up
-
-# north:
-iii = lat >  50
-paxn_B.contourf(lat[iii], mlt[iii], Br[iii], **B_kwargs)
-paxn_j.contour( lat[iii], mlt[iii], eq_current_function[iii], **eqJ_kwargs)
-paxn_j.contourf(lat[iii], mlt[iii], FAC[iii], **FAC_kwargs)
-
-# south:
-iii = lat < -50
-paxs_B.contourf(lat[iii], mlt[iii], Br[iii], **B_kwargs)
-paxs_j.contour( lat[iii], mlt[iii], eq_current_function[iii], **eqJ_kwargs)
-paxs_j.contourf(lat[iii], mlt[iii], FAC[iii], **FAC_kwargs)
-
-
-# scatter plot high latitude FACs
-iii = np.abs(i2d.state.num_grid.lat) > i2d.state.latitude_boundary
-jrmax = np.max(np.abs(i2d.state.jr))
-ax_1.scatter(i2d.state.jr, jr_mod[iii])
-ax_1.plot([-jrmax, jrmax], [-jrmax, jrmax], 'k-')
-ax_1.set_xlabel('Input ')
-
-# scatter plot FACs at conjugate points
-j_par_ll = i2d.state.G_par_ll.dot(i2d.state.TB.coeffs)
-j_par_ll_conj = i2d.state.G_par_ll_conj.dot(i2d.state.TB.coeffs)
-j_par_max = np.max(np.abs(j_par_ll))
-ax_2.scatter(j_par_ll, j_par_ll_conj)
-ax_2.plot([-j_par_max, j_par_max], [-j_par_max, j_par_max], 'k-')
-ax_2.set_xlabel('$j_\parallel$ [A/m$^2$] at |latitude| $< {}^\circ$'.format(i2d.state.latitude_boundary))
-ax_2.set_ylabel('$j_\parallel$ [A/m$^2$] at conjugate points')
-
-# scatter plot of ve1 and ve2 vs corresponding values at conjugate points
-cu_ll = (i2d.state.u_phi_ll_conj * i2d.state.c_u_phi_conj + i2d.state.u_theta_ll_conj * i2d.state.c_u_theta_conj).flatten()
-cu_co = (i2d.state.u_phi_ll * i2d.state.c_u_phi + i2d.state.u_theta_ll * i2d.state.c_u_theta).flatten()
-AT_ll = i2d.state.etaP_ll * i2d.state.A_eP_T + i2d.state.etaH_ll * i2d.state.A_eH_T
-AT_co = i2d.state.etaP_ll_conj * i2d.state.A_eP_conj_T + i2d.state.etaH_ll_conj * i2d.state.A_eH_conj_T 
-AV_ll = -(i2d.state.etaP_ll * i2d.state.A_eP_V + i2d.state.etaH_ll * i2d.state.A_eH_V)
-AV_co = -(i2d.state.etaP_ll_conj * i2d.state.A_eP_conj_V + i2d.state.etaH_ll_conj * i2d.state.A_eH_conj_V)
-
-c_ll = cu_ll + AV_ll.dot(i2d.state.VB.coeffs)
-c_co = cu_co + AV_co.dot(i2d.state.VB.coeffs)
-ve1_ll, ve2_ll = np.split(c_ll + AT_ll.dot(i2d.state.TB.coeffs), 2)
-ve1_co, ve2_co = np.split(c_co + AT_co.dot(i2d.state.TB.coeffs), 2)
-ax_3.scatter(ve1_ll, ve1_co, label = '$v_{e_1}$')
-ax_3.scatter(ve2_ll, ve2_co, label = '$v_{e_2}$')
-ax_3.set_xlabel('$v_{e_i}$')
-ax_3.set_ylabel('$v_{e_i}$ at conjugate points')
-ax_3.legend(frameon = False)
-
-
-
-plt.subplots_adjust(top=0.89, bottom=0.095, left=0.025, right=0.95, hspace=0.0, wspace=0.185)
-
-plt.show()
+debugplot(i2d, title = 'hoi!', filename = None, noon_longitude = noon_longitude)
 
 
 
