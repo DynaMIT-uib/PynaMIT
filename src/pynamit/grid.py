4d063d20374cfdba27833cdb1d69c62f6b4e3533
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-05-28 15:08:44 +0200

    Change internal name to field

--- src/pynamit/grid.py
+++ src/pynamit/grid.py
@@ -5,7 +5,7 @@ class Grid(object):
 
     """
 
-    def __init__(self, lat, lon, mainfield = None):
+    def __init__(self, lat, lon, field = None):
         """ Initialize the grid for the ionosphere.
 
         """
@@ -18,8 +18,8 @@ class Grid(object):
         self.theta = 90 - self.lat
 
         # Get magnetic field unit vectors and inclination at grid:
-        if mainfield is not None:
-            B = np.vstack(mainfield.get_B(self.theta, self.lon))
+        if field is not None:
+            B = np.vstack(field.get_B(self.theta, self.lon))
             self.B_magnitude = np.linalg.norm(B, axis = 0)
             self.br, self.btheta, self.bphi = B / self.B_magnitude
             self.sinI = -self.br / np.sqrt(self.btheta**2 + self.bphi**2 + self.br**2) # sin(inclination)
