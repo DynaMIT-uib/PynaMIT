Fix sign of wind interpolation

src/pynamit/pynamit.py
@@ -219,7 +219,7 @@ class I2D(object):
                     print('Saved output at t = {:.2f} s'.format(time), end = '\r')
 
 
-    def set_FAC(self, FAC, _basis_evaluator, _field_evaluator, time = None):
+    def set_FAC(self, FAC, _basis_evaluator, time = None):
         """
         Specify field-aligned current at ``self.num_grid.theta``,
         ``self.num_grid.lon``.
@@ -236,7 +236,6 @@ class I2D(object):
 
         self.FAC = np.atleast_2d(FAC)
         self.FAC_basis_evaluator = _basis_evaluator
-        self.FAC_field_evaluator = _field_evaluator
 
         if time is None:
             if self.FAC.shape[0] > 1:
@@ -296,7 +295,7 @@ class I2D(object):
 
         if self.next_FAC < self.FAC_time.size:
             if self.latest_time >= self.FAC_time[self.next_FAC]:
-                self.state.set_FAC(self.FAC[self.next_FAC], self.FAC_basis_evaluator, self.FAC_field_evaluator)
+                self.state.set_FAC(self.FAC[self.next_FAC], self.FAC_basis_evaluator)
                 self.next_FAC += 1
                 self.updated_FAC = True
 

src/pynamit/state.py
@@ -269,22 +269,24 @@ class State(object):
 
         self.neutral_wind = True
 
-        self.u_theta_on_grid = csp.interpolate_scalar(u_theta, _basis_evaluator.grid.theta, _basis_evaluator.grid.lon, self.num_grid.theta, self.num_grid.lon)
-        self.u_phi_on_grid   = csp.interpolate_scalar(u_phi,   _basis_evaluator.grid.theta, _basis_evaluator.grid.lon, self.num_grid.theta, self.num_grid.lon)
+        u_on_grid = csp.interpolate_vector_components(u_phi, -u_theta, np.zeros_like(u_phi), _basis_evaluator.grid.theta, _basis_evaluator.grid.lon, self.num_grid.theta, self.num_grid.lon)
+        self.u_theta_on_grid = -u_on_grid[1]
+        self.u_phi_on_grid   = u_on_grid[0]
 
         # Represent as expansion in spherical harmonics
-        #self.u_theta_on_grid, self.u_phi_on_grid = self.basis_evaluator.basis_to_grid( _basis_evaluator.grid_to_basis((u_theta, u_phi), helmholtz = True), helmholtz = True)
+        #self.u_theta_on_grid, self.u_phi_on_grid = self.basis_evaluator.basis_to_grid(_basis_evaluator.grid_to_basis((u_theta, u_phi), helmholtz = True), helmholtz = True)
 
         self.uxB_theta =  self.u_phi_on_grid   * self.b_evaluator.Br
         self.uxB_phi   = -self.u_theta_on_grid * self.b_evaluator.Br
 
         if self.connect_hemispheres:
             # Neutral wind at conjugate grid points
-            u_theta_on_cp_grid = csp.interpolate_scalar(u_theta, _basis_evaluator.grid.theta, _basis_evaluator.grid.lon, self.cp_grid.theta, self.cp_grid.lon)
-            u_phi_on_cp_grid   = csp.interpolate_scalar(u_phi,   _basis_evaluator.grid.theta, _basis_evaluator.grid.lon, self.cp_grid.theta, self.cp_grid.lon)
+            u_on_cp_grid = csp.interpolate_vector_components(u_phi, -u_theta, np.zeros_like(u_phi), _basis_evaluator.grid.theta, _basis_evaluator.grid.lon, self.cp_grid.theta, self.cp_grid.lon)
+            u_theta_on_cp_grid = -u_on_cp_grid[1]
+            u_phi_on_cp_grid   = u_on_cp_grid[0]
 
             # Represent as expansion in spherical harmonics
-            #self.u_theta_on_cp_grid, self.u_phi_on_cp_grid = self.cp_basis_evaluator.basis_to_grid( _basis_evaluator.grid_to_basis((u_theta, u_phi), helmholtz = True), helmholtz = True)
+            #self.u_theta_on_cp_grid, self.u_phi_on_cp_grid = self.cp_basis_evaluator.basis_to_grid(_basis_evaluator.grid_to_basis((u_theta, u_phi), helmholtz = True), helmholtz = True)
 
             # Neutral wind at low latitude grid points and at their conjugate points
             u_theta_ll    = self.u_theta_on_grid[self.ll_mask]