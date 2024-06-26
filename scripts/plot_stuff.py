027cf664ecd65381a39a40001a4a551f6732d311
Karl Laundal <karl.laundal@ift.uib.no>
2024-06-26 18:48:04 +0200

    script for experimenting with PynamEye

---
+++ scripts/plot_stuff.py
@@ -0,0 +1,19 @@
+# visualization
+
+import pynamit
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = '/Users/laundal/Dropbox/git/dynamit/PynaMIT/scripts/hdtest'
+a = pynamit.PynamEye(path).set_time(212)
+
+a.make_multipanel_output_figure()
+plt.savefig('fig_212.png')
+
+m_ss = np.load('/Users/laundal/Dropbox/git/dynamit/PynaMIT/notebooks/mvss.npy')
+a.m_ind = m_ss
+a.make_multipanel_output_figure()
+plt.savefig('fig_ss.png')
+
+
+
