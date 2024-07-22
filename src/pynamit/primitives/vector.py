2f6479b09f09434624bb5bc7452de1b54f7c0d1c
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-07-22 09:13:48 +0200

    Vector cleanup

--- src/pynamit/primitives/vector.py
+++ src/pynamit/primitives/vector.py
@@ -1,21 +1,27 @@
 class Vector(object):
 
     def __init__(self, basis, coeffs = None, basis_evaluator = None, grid_values = None, type = 'scalar'):
+        if type not in ['scalar', 'tangential']:
+            raise ValueError("Type must be either 'scalar' or 'tangential'.")
+
         self.basis = basis
         self._type = type
 
         if coeffs is not None:
             self.coeffs = coeffs
         elif (basis_evaluator is not None) and (grid_values is not None):
-            if self._type == 'scalar':
-                self.coeffs = basis_evaluator.grid_to_basis(grid_values, helmholtz = False)
-            elif self._type == 'tangential':
-                self.coeffs = basis_evaluator.grid_to_basis(grid_values, helmholtz = True)
-            else:
-                raise ValueError("Type must be either 'scalar' or 'tangential'.")
+            self.coeffs = self.coeffs_from_grid(basis_evaluator, grid_values)
         else:
-            raise ValueError("Either coeffs or grid and grid values must be provided.")
- 
+            raise ValueError("Either coeffs or basis evaluator and grid values must be provided.")
+
+
+    def coeffs_from_grid(self, basis_evaluator, grid_values):
+        if self._type == 'scalar':
+            return basis_evaluator.grid_to_basis(grid_values, helmholtz = False)
+        elif self._type == 'tangential':
+            return basis_evaluator.grid_to_basis(grid_values, helmholtz = True)
+
+
     def to_grid(self, basis_evaluator):
         if self._type == 'scalar':
             return basis_evaluator.basis_to_grid(self.coeffs, helmholtz = False)
