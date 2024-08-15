ef5151280140396b02aed1bfa0e4a1f6116f94e1
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-08-15 10:31:47 +0200

    Forgot math routine

--- src/pynamit/various/math.py
+++ src/pynamit/various/math.py
@@ -7,7 +7,7 @@ def inv_and_cond_hermitian(A):
 
     u, s, vh = np.linalg.svd(A, hermitian = True)
 
-    print('The condition number for the matrix is {:.1f}'.format(s[0] / s[-1]))
+    #print('The condition number for the matrix is {:.1f}'.format(s[0] / s[-1]))
 
     return np.einsum('ij, j, jk -> ik', vh.T, 1 / s, u.T)
     #return np.dot(vh.T / s, u.T) # Need to compare relative speed of einsum and broadcast + dot
