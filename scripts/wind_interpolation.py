148399aa6265f601689259dfac5fefe9e0010415
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-08-28 12:05:54 +0200

    Further improve wind interpolation script

--- scripts/wind_interpolation.py
+++ scripts/wind_interpolation.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
 PLOT = True
-PROJECTED_CS = True
-MAX_NMAX_MMAX = 30
+SH_COMPARISON = True
+GRID_COMPARISON = True
+MAX_NMAX_MMAX = 60
 
 rtol = 1e-15
 Ncs = 70
@@ -18,8 +19,11 @@ d = dipole.Dipole(date.year)
 nooNmax_Mmaxlon = d.mlt2mlon(12, date) # noon longitude
 
 ## WIND INPUT
-hwm14Obj = pyhwm2014.HWM142D(alt=110., ap=[35, 35], glatlim=[-89., 88.], glatstp = 3., 
-                             glonlim=[-180., 180.], glonstp = 8., option = 6, verbose = False, ut = date.hour + date.minute/60, day = date.timetuple().tm_yday)
+#hwm14Obj = pyhwm2014.HWM142D(alt=110., ap=[35, 35], glatlim=[-89., 88.], glatstp = 3.,
+#                             glonlim=[-180., 180.], glonstp = 8., option = 6, verbose = False, ut = date.hour + date.minute/60, day = date.timetuple().tm_yday)
+
+hwm14Obj = pyhwm2014.HWM142D(alt=110., ap=[35, 35], glatlim=[-88.5, 88.5], glatstp = 1.5,
+                             glonlim=[-180., 180.], glonstp = 3., option = 6, verbose = False, ut = date.hour + date.minute/60, day = date.timetuple().tm_yday)
 
 u_theta, u_phi = (-hwm14Obj.Vwind.flatten(), hwm14Obj.Uwind.flatten())
 u_lat, u_lon = np.meshgrid(hwm14Obj.glatbins, hwm14Obj.glonbins, indexing = 'ij')
@@ -35,52 +39,60 @@ interpolated_data = np.hstack((-interpolated_north, interpolated_east)) # conver
 lon = csp_grid.lon.flatten()
 lat = csp_grid.lat.flatten()
 
-relative_grid_errors = []
-if PROJECTED_CS:
+if GRID_COMPARISON:
+    relative_grid_errors = []
+if SH_COMPARISON:
     relative_coeff_errors = []
 
 
-for Nmax_Mmax in range(1, MAX_NMAX_MMAX + 1):
+for Nmax_Mmax in range(10, MAX_NMAX_MMAX + 1, 10):
     sh_basis = pynamit.SHBasis(Nmax_Mmax, Nmax_Mmax)
     input_basis_evaluator = pynamit.BasisEvaluator(sh_basis, u_grid, pinv_rtol = rtol, weights = np.sin(np.deg2rad(90 - u_lat.flatten())))
     state_basis_evaluator = pynamit.BasisEvaluator(sh_basis, csp_grid, pinv_rtol = rtol)
 
     sh_interpolated_u = pynamit.Vector(sh_basis, basis_evaluator = input_basis_evaluator, grid_values = np.hstack((u_theta, u_phi)), type = 'tangential')
-    sh_interpolated_u_on_grid = sh_interpolated_u.to_grid(state_basis_evaluator)
 
-    if PROJECTED_CS:
-        cs_interpolated_u = pynamit.Vector(sh_basis, basis_evaluator = state_basis_evaluator, grid_values = interpolated_data, type = 'tangential')
-        cs_interpolated_u_on_grid = cs_interpolated_u.to_grid(state_basis_evaluator)
-    else:
+    print("Interpolation with Nmax = %d, Mmax = %d:" % (Nmax_Mmax, Nmax_Mmax))
+
+    if GRID_COMPARISON:
         cs_interpolated_u_on_grid = interpolated_data
+        sh_interpolated_u_on_grid = sh_interpolated_u.to_grid(state_basis_evaluator)
+        relative_grid_errors.append(np.linalg.norm(cs_interpolated_u_on_grid - sh_interpolated_u_on_grid)/np.linalg.norm(cs_interpolated_u_on_grid))
+        print("   Relative grid error = %e" % (relative_grid_errors[-1]))
+
+    if SH_COMPARISON:
+        cs_interpolated_u = pynamit.Vector(sh_basis, basis_evaluator = state_basis_evaluator, grid_values = interpolated_data, type = 'tangential')
+        relative_coeff_errors.append(np.linalg.norm(cs_interpolated_u.coeffs - sh_interpolated_u.coeffs)/np.linalg.norm(cs_interpolated_u.coeffs))
+        print("   Relative coefficient error = %e" % (relative_coeff_errors[-1]))
 
     if PLOT:
-        ## Prepare for plotting
-        grid_fig, (grid_cs_ax, grid_sh_ax) = plt.subplots(1, 2, figsize=(20, 5), layout = 'constrained', subplot_kw={'projection': ccrs.PlateCarree(central_longitude = nooNmax_Mmaxlon)})
-        grid_cs_ax.coastlines()
-        grid_sh_ax.coastlines()
+        if GRID_COMPARISON:
+            grid_fig, (grid_cs_ax, grid_sh_ax) = plt.subplots(1, 2, figsize=(20, 5), layout = 'constrained', subplot_kw={'projection': ccrs.PlateCarree(central_longitude = nooNmax_Mmaxlon)})
+            grid_cs_ax.coastlines()
+            grid_sh_ax.coastlines()
 
-        grid_cs_ax.title.set_text("Cubed sphere")
-        grid_sh_ax.set_title("Spherical harmonics")
+            grid_cs_ax.title.set_text("Cubed sphere")
+            grid_sh_ax.set_title("Spherical harmonics")
 
-        # Plot grid wind field
-        cs_quiver = grid_cs_ax.quiver(lon, lat, np.split(cs_interpolated_u_on_grid, 2)[1].flatten(), -np.split(cs_interpolated_u_on_grid, 2)[0].flatten(), color='blue', transform=ccrs.PlateCarree())
-        grid_sh_ax.quiver(lon, lat, np.split(sh_interpolated_u_on_grid, 2)[1].flatten(), -np.split(sh_interpolated_u_on_grid, 2)[0].flatten(), color='red', scale = cs_quiver.scale, transform=ccrs.PlateCarree())
+            # Plot grid wind field
+            cs_quiver = grid_cs_ax.quiver(lon, lat, np.split(cs_interpolated_u_on_grid, 2)[1].flatten(), -np.split(cs_interpolated_u_on_grid, 2)[0].flatten(), color='blue', transform=ccrs.PlateCarree())
+            grid_sh_ax.quiver(lon, lat, np.split(sh_interpolated_u_on_grid, 2)[1].flatten(), -np.split(sh_interpolated_u_on_grid, 2)[0].flatten(), color='red', scale = cs_quiver.scale, transform=ccrs.PlateCarree())
 
-        plt.show()
+            plt.show()
 
-        if PROJECTED_CS:
-            coeff_fig, (coeff_cs_ax, coeff_sh_ax) = plt.subplots(1, 2, figsize=(20, 5))
+        if SH_COMPARISON:
+            coeff_fig, (coeff_cs_ax, coeff_sh_ax) = plt.subplots(1, 2, figsize=(20, 5), layout = 'constrained')
             abs_coeff_cs = np.abs(cs_interpolated_u.coeffs)
             abs_coeff_sh = np.abs(sh_interpolated_u.coeffs)
 
+            coeff_cs_ax.set_title("Cubed sphere coefficient magnitudes")
+            coeff_sh_ax.set_title("Spherical harmonics coefficient magnitudes")
+
+            # Plot curl free and divergence free coefficients
             coeff_cs_ax.plot(np.split(abs_coeff_cs, 2)[0], label = "CF")
             coeff_cs_ax.plot(np.split(abs_coeff_cs, 2)[1], label = "DF")
             coeff_sh_ax.plot(np.split(abs_coeff_sh, 2)[0], label = "CF")
             coeff_sh_ax.plot(np.split(abs_coeff_sh, 2)[1], label = "DF")
-
-            coeff_cs_ax.set_title("Cubed sphere coefficient magnitudes")
-            coeff_sh_ax.set_title("Spherical harmonics coefficient magnitudes")
 
             min_coeff = min(np.min(abs_coeff_cs), np.min(abs_coeff_sh))
             max_coeff = max(np.max(abs_coeff_cs), np.max(abs_coeff_sh))
@@ -92,24 +104,17 @@ for Nmax_Mmax in range(1, MAX_NMAX_MMAX + 1):
 
             coeff_cs_ax.set_yscale("log")
             coeff_sh_ax.set_yscale("log")
+
             plt.show()
 
-    relative_grid_errors.append(np.linalg.norm(cs_interpolated_u_on_grid - sh_interpolated_u_on_grid)/np.linalg.norm(cs_interpolated_u_on_grid))
-    if PROJECTED_CS:
-        relative_coeff_errors.append(np.linalg.norm(cs_interpolated_u.coeffs - sh_interpolated_u.coeffs)/np.linalg.norm(cs_interpolated_u.coeffs))
-
-    if PROJECTED_CS:
-        print("Finished interpolation with Nmax = %d, Mmax = %d, relative grid error = %e, relative coefficient error = %e" % (Nmax_Mmax, Nmax_Mmax, relative_grid_errors[-1], relative_coeff_errors[-1]))
-    else:
-        print("Finished interpolation with Nmax = %d, Mmax = %d, relative grid error = %e" % (Nmax_Mmax, Nmax_Mmax, relative_grid_errors[-1]))
-
 # Plot errors
-plt.plot(relative_grid_errors, label = "Grid values")
-plt.yscale("log")
-plt.xlabel("Nmax = Mmax")
-plt.ylabel("Error (relative to CS interpolation)")
+if GRID_COMPARISON:
+    plt.plot(relative_grid_errors, label = "Grid values")
+    plt.yscale("log")
+    plt.xlabel("Nmax = Mmax")
+    plt.ylabel("Error (relative to CS interpolation)")
 
-if PROJECTED_CS:
+if SH_COMPARISON:
     plt.plot(relative_coeff_errors, label = "Coefficients")
     plt.yscale("log")
     plt.xlabel("Nmax = Mmax")
