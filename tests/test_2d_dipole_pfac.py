e1ff5a85e5dd3ee3b7937ea67fac3b302809fb4e
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-08-29 09:22:43 +0200

    Loosen sp tests to 1e-7, tighten dp tests to 1e-11

--- tests/test_2d_dipole_pfac.py
+++ tests/test_2d_dipole_pfac.py
@@ -39,7 +39,7 @@ def test_2d_dipole_pfac():
     print("actual_coeff_min: ", actual_coeff_min)
     print("actual_n_coeffs: ", actual_n_coeffs)
 
-    assert actual_coeff_norm == pytest.approx(expected_coeff_norm, abs=0.0, rel=1e-10)
-    assert actual_coeff_max == pytest.approx(expected_coeff_max, abs=0.0, rel=1e-10)
-    assert actual_coeff_min == pytest.approx(expected_coeff_min, abs=0.0, rel=1e-10)
-    assert actual_n_coeffs == pytest.approx(expected_n_coeffs, abs=0.0, rel=1e-10)
+    assert actual_coeff_norm == pytest.approx(expected_coeff_norm, abs=0.0, rel=1e-11)
+    assert actual_coeff_max == pytest.approx(expected_coeff_max, abs=0.0, rel=1e-11)
+    assert actual_coeff_min == pytest.approx(expected_coeff_min, abs=0.0, rel=1e-11)
+    assert actual_n_coeffs == pytest.approx(expected_n_coeffs, abs=0.0, rel=1e-11)
--- tests/test_2d_igrf_pfac_hc_wind.py
+++ tests/test_2d_igrf_pfac_hc_wind.py
@@ -48,7 +48,7 @@ def test_2d_igrf_pfac_hc_wind():
     print("actual_coeff_min: ", actual_coeff_min)
     print("actual_n_coeffs: ", actual_n_coeffs)
 
-    assert actual_coeff_norm == pytest.approx(expected_coeff_norm, abs=0.0, rel=1e-9)
-    assert actual_coeff_max == pytest.approx(expected_coeff_max, abs=0.0, rel=1e-9)
-    assert actual_coeff_min == pytest.approx(expected_coeff_min, abs=0.0, rel=1e-9)
-    assert actual_n_coeffs == pytest.approx(expected_n_coeffs, abs=0.0, rel=1e-9)
+    assert actual_coeff_norm == pytest.approx(expected_coeff_norm, abs=0.0, rel=1e-7)
+    assert actual_coeff_max == pytest.approx(expected_coeff_max, abs=0.0, rel=1e-7)
+    assert actual_coeff_min == pytest.approx(expected_coeff_min, abs=0.0, rel=1e-7)
+    assert actual_n_coeffs == pytest.approx(expected_n_coeffs, abs=0.0, rel=1e-7)
