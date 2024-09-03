155d7f18e6a3d1331e23f91df31e1a2ca462bcd5
andreasskeidsvoll <andreas@skeidsvoll.no>
2024-09-03 14:06:18 +0200

    Expanded steady state calculation

--- src/pynamit/simulation/dynamics.py
+++ src/pynamit/simulation/dynamics.py
@@ -565,6 +565,12 @@ class Dynamics(object):
         E_imp = G_m_imp_to_E.dot(self.state.m_imp.coeffs)
         curl_matrix = self.sh_curl_matrix
 
-        m_ind = np.linalg.pinv(curl_matrix.dot(G_m_ind_to_E)).dot(curl_matrix.dot(self.state.uxB - E_imp))
+        #m_ind = np.linalg.pinv(curl_matrix.dot(G_m_ind_to_E)).dot(curl_matrix.dot(self.state.uxB - E_imp))
+
+        G_imp_inv = np.linalg.pinv(self.state.G_m_imp_constraints)
+
+        m_ind = np.linalg.pinv(curl_matrix.dot(G_m_ind_to_E)).dot(curl_matrix.dot(self.state.uxB)) \
+              - np.linalg.pinv(curl_matrix.dot(G_m_ind_to_E)).dot(curl_matrix.dot(G_m_imp_to_E.dot(G_imp_inv.dot(np.hstack((self.state.jr_on_grid, self.state.cu * self.state.ih_constraint_scaling))) \
+                                                                                                   + G_imp_inv.dot(np.hstack((np.zeros(self.state.jr_on_grid.shape[0]), self.state.A_ind.dot(self.state.m_ind.coeffs) * self.state.ih_constraint_scaling))))))
 
         return(m_ind)
