348fb39745f5afec387199ed0fd669cb94162a0b
andreasskeidsvoll <andreas@skeidsvoll.no>
2025-02-13 15:35:51 +0100

    Continue improving vector documentation

--- src/pynamit/primitives/vector.py
+++ src/pynamit/primitives/vector.py
@@ -31,6 +31,12 @@ class Vector(object):
 
 
     def coeffs_from_grid(self, basis_evaluator, grid_values):
+        """
+        Perform the inversion to compute the basis expansion coefficients
+        from the grid values.
+
+        """
+
         if self._type == 'scalar':
             return basis_evaluator.grid_to_basis(grid_values, helmholtz = False)
         elif self._type == 'tangential':
@@ -38,6 +44,12 @@ class Vector(object):
 
 
     def to_grid(self, basis_evaluator):
+        """
+        Evaluate the basis expansion on a grid, where the mapping between
+        the basis and the grid is provided by the `basis_evaluator` object.
+
+        """
+
         if self._type == 'scalar':
             return basis_evaluator.basis_to_grid(self.coeffs, helmholtz = False)
         elif self._type == 'tangential':
@@ -45,6 +57,12 @@ class Vector(object):
 
 
     def regularization_term(self, basis_evaluator):
+        """
+        Return the regularization term for the inversion used to compute
+        the basis expansion coefficients.
+
+        """
+
         if self._type == 'scalar':
             return basis_evaluator.regularization_term(self.coeffs, helmholtz = False)
         elif self._type == 'tangential':
