d1beb8687d911a214d26ba2d6ad3e973cd3b96d6
andreasskeidsvoll <andreas@skeidsvoll.no>
2025-02-15 11:47:18 +0100

    Sort imports and use the __all__ construct

--- src/pynamit/__init__.py
+++ src/pynamit/__init__.py
@@ -5,13 +5,27 @@ with dynamics induced by field-aligned currents and horizontal neutral winds.
 This package includes modules for simulation, visualization, and various utilities.
 """
 
-from .simulation.dynamics import Dynamics
-from .simulation.visualization import globalplot, debugplot
-from .simulation.pynameye import PynamEye
 from .cubed_sphere.cubed_sphere import CSProjection
-from .spherical_harmonics.sh_basis import SHBasis
-from .simulation.mainfield import Mainfield
-from .primitives.field_evaluator import FieldEvaluator
 from .primitives.basis_evaluator import BasisEvaluator
+from .primitives.field_evaluator import FieldEvaluator
 from .primitives.grid import Grid
 from .primitives.vector import Vector
+from .simulation.dynamics import Dynamics
+from .simulation.mainfield import Mainfield
+from .simulation.pynameye import PynamEye
+from .simulation.visualization import debugplot, globalplot
+from .spherical_harmonics.sh_basis import SHBasis
+
+__all__ = [
+    "BasisEvaluator",
+    "CSProjection",
+    "Dynamics",
+    "FieldEvaluator",
+    "Grid",
+    "Mainfield",
+    "PynamEye",
+    "SHBasis",
+    "Vector",
+    "debugplot",
+    "globalplot"
+]
