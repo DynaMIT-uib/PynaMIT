01a0400da2962664728f2d66b78a2c8964763da7
andreasskeidsvoll <andreas@skeidsvoll.no>
2025-02-20 15:12:32 +0100

    Make conf conform to pep

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,24 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
 
-# -- Path setup --------------------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#path-setup
+For the full list of built-in configuration values, see the
+documentation:
 
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+# Path setup
 import os
 import sys
 
 # Add the project root directory to the system path
 sys.path.insert(0, os.path.abspath("../../"))
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# Project information
 project = "PynaMIT"
 copyright = "2024, PynaMIT Developers"
 author = "PynaMIT Developers"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# General configuration
 extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.autodoc",
@@ -34,13 +31,11 @@ exclude_patterns = []
 
 nitpicky = True
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# Options for HTML output
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
-# -- Options for autodoc -----------------------------------------------------
+# Options for autodoc
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
 
 autodoc_default_options = {
@@ -51,12 +46,12 @@ autodoc_default_options = {
     "show-inheritance": True,
 }
 
-# -- Options for napoleon ----------------------------------------------------
+# Options for napoleon
 # https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
 
 napoleon_google_docstring = False
 
-# -- Options for myst_parser -------------------------------------------------
+# Options for myst_parser
 # https://myst-parser.readthedocs.io/en/latest/index.html
 
 source_suffix = {
