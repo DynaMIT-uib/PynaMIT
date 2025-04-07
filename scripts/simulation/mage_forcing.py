e6f1f94614dd83dde9a1d4bc2416d3801c427d92
andreasskeidsvoll <andreas@skeidsvoll.no>
2025-04-07 11:37:57 +0200

    Plotting interpolated field

--- scripts/simulation/mage_forcing.py
+++ scripts/simulation/mage_forcing.py
@@ -12,21 +12,17 @@ from polplot import Polarplot
 import matplotlib.pyplot as plt
 
 
-RE = 6371.2e3
-RI = RE + 110e3
-latitude_boundary = 40
+RE = 6381e3
+RI = 6.5e3
+latitude_boundary = 35
 dt = 300
-
-FLOAT_ERROR_MARGIN = 1e-6
 
 dataset_filename_prefix = "mage-forcing"
 Nmax, Mmax, Ncs = 30, 30, 30
 rk = RI / np.cos(np.deg2rad(np.r_[0:70:2])) ** 2
 
-date = datetime.datetime(2001, 5, 12, 17, 0)
+date = datetime.datetime(2013, 3, 17, 10)
 d = dipole.Dipole(date.year)
-noon_longitude = d.mlt2mlon(12, date)  # Noon longitude
-noon_mlon = d.mlt2mlon(12, date)  # Noon longitude
 
 # Set up simulation object.
 dynamics = pynamit.Dynamics(
@@ -113,19 +109,21 @@ for step in range(0, nstep):
 
     plotting = True
     if plotting:
+        dynamics.select_timeseries_data("jr")
+        minlat = 0
         # PLOTTING
         fig = plt.figure(figsize=(10, 6))
 
-        paxn_imposed = Polarplot(plt.subplot2grid((2, 4), (0, 0)))
-        paxs_imposed = Polarplot(plt.subplot2grid((2, 4), (0, 1)))
-        paxn_induced = Polarplot(plt.subplot2grid((2, 4), (0, 2)))
-        paxs_induced = Polarplot(plt.subplot2grid((2, 4), (0, 3)))
+        paxn_input = Polarplot(plt.subplot2grid((2, 4), (0, 0)), minlat=minlat)
+        paxs_input = Polarplot(plt.subplot2grid((2, 4), (0, 1)), minlat=minlat)
+        paxn_interpolated = Polarplot(plt.subplot2grid((2, 4), (0, 2)), minlat=minlat)
+        paxs_interpolated = Polarplot(plt.subplot2grid((2, 4), (0, 3)), minlat=minlat)
 
         global_projection = ccrs.PlateCarree(central_longitude=0)
-        gax_imposed = plt.subplot2grid((2, 2), (1, 0), projection=global_projection, rowspan=2)
-        gax_induced = plt.subplot2grid((2, 2), (1, 1), projection=global_projection, rowspan=2)
+        gax_input = plt.subplot2grid((2, 2), (1, 0), projection=global_projection, rowspan=2)
+        gax_interpolated = plt.subplot2grid((2, 2), (1, 1), projection=global_projection, rowspan=2)
 
-        for ax in [gax_imposed, gax_induced]:
+        for ax in [gax_input, gax_interpolated]:
             ax.coastlines(zorder=2, color="grey")
 
         lat = 90 - full_theta_centered
@@ -136,21 +134,48 @@ for step in range(0, nstep):
            "extend": "both",
         }
 
-        contours = {}
-        contours["jr "] = gax_imposed.contourf(
+        # jr input:
+        contours_input = {}
+        contours_input["jr "] = gax_input.contourf(
             lon, lat, full_current.reshape(lon.shape), transform=ccrs.PlateCarree(), **jr_kwargs
         )
 
         # north:
-        nnn = lat > 50
-        contours["jr_n"] = paxn_imposed.contourf(
+        nnn = lat > minlat
+        contours_input["jr_n"] = paxn_input.contourf(
             lat[nnn], lon[nnn] / 15, full_current.reshape(lon.shape)[nnn], **jr_kwargs
         )
 
         # south:
-        sss = lat < -50
-        contours["jr_s"] = paxs_imposed.contourf(
+        sss = lat < -minlat
+        contours_input["jr_s"] = paxs_input.contourf(
             lat[sss], lon[sss] / 15, full_current.reshape(lon.shape)[sss], **jr_kwargs
+        )
+
+        lat, lon = np.linspace(-89.9, 89.9, 60), np.linspace(-180, 180, 100)
+        lat, lon = np.meshgrid(lat, lon)
+        plt_grid = pynamit.Grid(lat=lat, lon=lon)
+        plt_evaluator = pynamit.BasisEvaluator(dynamics.state.jr_basis, plt_grid)
+        b_evaluator = pynamit.FieldEvaluator(dynamics.mainfield, plt_grid, RI)
+
+        fac_output = plt_evaluator.basis_to_grid(dynamics.state.jr.coeffs) / b_evaluator.br
+
+        # jr interpolated:
+        contours_interpolated = {}
+        contours_interpolated["jr "] = gax_interpolated.contourf(
+            lon, lat, fac_output.reshape(lon.shape), transform=ccrs.PlateCarree(), **jr_kwargs
+        )
+
+        # north:
+        nnn = lat > minlat
+        contours_interpolated["jr_n"] = paxn_interpolated.contourf(
+            lat[nnn], lon[nnn] / 15, fac_output.reshape(lon.shape)[nnn], **jr_kwargs
+        )
+
+        # south:
+        sss = lat < -minlat
+        contours_interpolated["jr_s"] = paxs_interpolated.contourf(
+            lat[sss], lon[sss] / 15, fac_output.reshape(lon.shape)[sss], **jr_kwargs
         )
 
         plt.show()
