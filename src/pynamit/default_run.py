Fix missing lambda scaling in LeastSquares (#75)
* Fix missing lambda scaling in LeastSquares

* Ruff format

src/pynamit/default_run.py
@@ -24,6 +24,9 @@ def run_pynamit(
     vector_conductance=True,
     vector_u=True,
     integrator="euler",
+    jr_lambda=None,
+    conductance_lambda=None,
+    u_lambda=None,
 ):
     """Run a default PynaMIT simulation with the given parameters.
 
@@ -63,6 +66,12 @@ def run_pynamit(
         Whether to use vector representation for wind.
     integrator : {'euler', 'exponential'}, optional
         Integrator type for time evolution.
+    jr_lambda : float, optional
+        Regularization parameter for the radial current.
+    conductance_lambda : float, optional
+        Regularization parameter for the conductance.
+    u_lambda : float, optional
+        Regularization parameter for the wind.
 
     Returns
     -------
@@ -108,7 +117,9 @@ def run_pynamit(
     hall, pedersen = conductance.hardy_EUV(
         conductance_lon, conductance_lat, Kp, date, starlight=1, dipole=True
     )
-    dynamics.set_conductance(hall, pedersen, lat=conductance_lat, lon=conductance_lon)
+    dynamics.set_conductance(
+        hall, pedersen, lat=conductance_lat, lon=conductance_lon, reg_lambda=conductance_lambda
+    )
 
     # Get and set jr input.
     jr_lat = dynamics.state_grid.lat
@@ -118,7 +129,7 @@ def run_pynamit(
     jr = a.get_upward_current(mlat=jr_lat, mlt=d.mlon2mlt(jr_lon, date)) * 1e-6
     # Filter low latitude jr.
     jr[np.abs(jr_lat) < 50] = 0
-    dynamics.set_jr(jr, lat=jr_lat, lon=jr_lon)
+    dynamics.set_jr(jr, lat=jr_lat, lon=jr_lon, reg_lambda=jr_lambda)
 
     # Get and set wind input.
     if wind:
@@ -143,6 +154,7 @@ def run_pynamit(
             lat=u_lat,
             lon=u_lon,
             weights=np.tile(np.sin(np.deg2rad(90 - u_lat.flatten())), (2, 1)),
+            reg_lambda=u_lambda,
         )
 
     if steady_state: